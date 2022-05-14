2227d7c26e2a4b47da8ef4a4996e7b44132ea8a3
AboodXD <aboood40091@gmail.com>
2022-05-14 05:22:01 +0400

    Wii U: Slightly optimize gu math

--- lib/src/guTranslateF.c
+++ lib/src/guTranslateF.c
@@ -7,7 +7,11 @@ void guTranslateF(float m[4][4], float x, float y, float z) {
     m[3][2] = z;
 }
 void guTranslate(Mtx *m, float x, float y, float z) {
+#if defined(NON_MATCHING) && defined(GBI_FLOATS)
+    guTranslateF(m->m, x, y, z);
+#else
     float mf[4][4];
     guTranslateF(mf, x, y, z);
     guMtxF2L(mf, m);
+#endif
 }
